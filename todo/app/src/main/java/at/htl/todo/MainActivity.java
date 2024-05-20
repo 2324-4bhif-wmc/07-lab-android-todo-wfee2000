9bde16f660c0c69b94ee0907165cf2b93c749ed2
wfee2000 <ilmingwinnie19@gmail.com>
2024-05-20 17:33:04 +0200

    feat: implement steps 1-7

---
+++ todo/app/src/main/java/at/htl/todo/MainActivity.java
@@ -0,0 +1,30 @@
+package at.htl.todo;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.ComponentActivity;
+
+import javax.inject.Inject;
+
+import at.htl.todo.ui.layout.MainView;
+import at.htl.todo.util.Config;
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class MainActivity extends ComponentActivity {
+    static final String TAG = MainActivity.class.getSimpleName();
+
+    @Inject
+    MainView mainView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Config.load(this);
+        var base_url = Config.getProperty("json.placeholder.baseurl");
+        Log.i(TAG, "onCreate: " + base_url);
+        mainView.buildContent(this);
+    }
+}
+
